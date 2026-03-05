9f7a71a8e9362047e23a61a6596c9299698ddd9f
Naing Khant Win <naingkhantab@gmail.com>
2026-03-05 14:43:23 +0630

    api_constrant

--- lib/core/network/api_constants.dart
+++ lib/core/network/api_constants.dart
@@ -1,15 +1,13 @@
 class ApiConstants {
-static const baseUrl = 'https://abw-business-card-backend.onrender.com/api';
+  static const baseUrl = 'https://abw-business-card-backend.onrender.com/api';
 
+  static const login = "login";
+  static const sendOtp = "send-otp";
+  static const verifyOtp = "verify-otp";
+  static const completeRegister = "complete-register";
+  static const logout = "logout";
+  static const me = "me";
 
-  static const login = "/login";
-  static const sendOtp = "/send-otp";
-  static const verifyOtp = "/verify-otp";
-  static const completeRegister = "/complete-register";
-  static const logout = "/logout";
-  static const me = "/me";
-
-  static const companies = '/companies';
-
-  static const cards = '/business-cards';
+  static const companies = 'companies';
+  static const cards = 'business-cards';
 }
